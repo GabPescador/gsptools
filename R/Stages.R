0401127bc33fb9678923753a6761e2c65891edc2
da Silva Pescador, Gabriel <139397018+GabPescador@users.noreply.github.com>
2025-05-09 15:59:28 -0500

    Added plotStages function

---
+++ R/Stages.R
@@ -0,0 +1,12 @@
+#' Stages
+#'
+#' Dataframe with stages data for plotStages().
+#'
+#' @format A data frame with 5 rows and 2 columns:
+#' \describe{
+#'   \item{class}{Number related to class}
+#'   \item{stage}{Human readable stage}
+#'   ...
+#' }
+#' @source Generated from classifier
+"Stages"
--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,24 @@
 #' @keywords internal
+#' @import ggplot2
 #' @importFrom OrgMassSpecR Digest
 #' @importFrom KEGGREST keggGet
 #' @importFrom magrittr `%>%`
 #' @importFrom utils head tail
 #' @importFrom dplyr filter
 #' @importFrom stats lm
+#' @importFrom stats fisher.test
+#' @importFrom plotrix std.error
+#' @importFrom readr read_csv
+#' @importFrom readr write_csv
+#' @importFrom readr parse_number
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr count
+#' @importFrom dplyr summarise
+#' @importFrom dplyr summarize
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom reshape2 dcast
+#' @importFrom forcats fct_relevel
+#' 
 NULL
