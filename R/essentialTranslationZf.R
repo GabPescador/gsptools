855eb13e56ee9717e7033af2d31708572b1cef68
da Silva Pescador, Gabriel <139397018+GabPescador@users.noreply.github.com>
2025-10-16 14:06:24 -0500

    Added essential translation factor list

---
+++ R/essentialTranslationZf.R
@@ -0,0 +1,14 @@
+#' Essential translation factors by Leo (Kostova Lab)
+#'
+#' Dataframe with essential translation factors.
+#'
+#' @format A data frame:
+#' \describe{
+#'   \item{gene_name}{Gene name by Leo}
+#'   \item{protein_name}{Protein name}
+#'   \item{protein_id}{Protein ID}
+#'   \item{description}{Leo's description of function}
+#'   ...
+#' }
+#' @source Got from Leo in Kostova Lab.
+"essentialTranslationZf"
